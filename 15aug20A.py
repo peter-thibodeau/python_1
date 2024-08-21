c1f31ed164e31669161e718179263ff986eb3121
Peter Thibodeau <158618486+peter-thibodeau@users.noreply.github.com>
2024-08-21 15:50:00 -0400

    Update 15aug20A.py

--- 15aug20A.py
+++ 15aug20A.py
@@ -1,28 +1,24 @@
 print('IS IT A PALINDROME?')
 
 # input
-inp = input('Enter a word, phrase, letters, or numbers: ')
+inp = input('Enter word, phrase, letters, or numbers: ')
 
-# create variables
+# processing and printing result
+lwr = inp.lower()
+no_spaces = lwr.replace(" ", "")
 l1 = len(no_spaces)
-l2 = len(no_spaces) // 2 # handle input whose length is odd
+l2 = len(no_spaces) // 2
 x = 0
+flag = ''
 
-def proc_inp(inp):
-    """ normalize input """
-    lwr = inp.lower()
-    return lwr.replace(" ", "")
+def is_palindrome(no_spaces, x):
+    """ determine if input is a palindrome """
+    if x == l2:
+        print(inp, 'is a palindrome')
+    elif no_spaces[x] == no_spaces[l1-x-1]:
+        is_palindrome(no_spaces, x+1)
+    else:
+        print(inp, 'is not a palindrome')
 
-def is_palindrome(no_spaces):
-    """ determine if input is a palindrome"""
-    for i in range (0, l2):
-        if no_spaces[i] == no_spaces[l1-i-1]:
-            x += 1
-        else:
-            print(inp, 'is not a palindrome.')
-            quit()
-
-no_spaces = proc_inp(inp)
-
-if x == l2: # passed
-    print(inp, 'is a palindrome')
+# call function to start processing
+is_palindrome(no_spaces, x)
